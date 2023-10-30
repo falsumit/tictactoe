9042d9d7fbc3f4d4d9bb0d07fffc807b7d16eebf
falsumit <bclarke516@outlook.com>
2023-10-30 14:27:02 -0400

    game board saved to array in object, user saved to object from form entry

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 Instructions:
 1) 
-    A) Store game board as an array inside an object
-    B) Players also stored in objects
+    A) Store game board as an array inside an object --DONE
+    B) Players also stored in objects --DONE
     C) An object to control the flow of the game
 
 - Have a little (if any) global code as possible
@@ -33,69 +33,88 @@ Instructions:
 
 */
 
-(function() {
-    const game = {
-        board: []
-    }
+// Game board
 
-    game.board.push('hello', 'world');
+(function() {
+    const game = (function() {
+        const board = [];
+        const spaces = document.querySelectorAll('.space');
+        for(let i = 0; i < spaces.length; i++) {
+            board.push(spaces[i]);
+        }
+    
+        return { board }
+    })();
+    
     console.log(game);
-    return game
+    console.log(game.board);
 })();
 
-function createUser(name) {
-    const userName = name;
-    let userScore = 0;
-    const getUserScore = () => userScore;
-    const giveUserScore = () => userScore++;
-
-    return { userName, userScore, getUserScore, giveUserScore }
-}
-
-function createOp(name) {
-    const opName = name;
-    let opScore = 0;
-    const getOpScore = () => opScore;
-    const giveOpScore = () => opScore++;
-
-    return { opName, opScore, getOpScore, giveOpScore }
-}
+// Users
 
 (function() {
-    const start = document.querySelector('button');
-    start.addEventListener('click', (name) => {
-        name = prompt('Enter your name');
-        const newUser = createUser(name);
-        const newOp = createOp('Opponent');
-        console.log(`${newUser.userName}'s Score: ${newUser.userScore}`);
-        console.log(`${newOp.opName}'s Score: ${newOp.opScore}`);
-    });
+    const form = document.getElementById('start');
+    form.addEventListener('submit', createUsers);
 })();
 
-function selectSpace (choice) {
-    const spaces = document.querySelectorAll('.space');
+function createUsers(event) {
+    event.preventDefault();
 
-    do {
-        choice = (Math.floor(Math.random() * spaces.length));
-    } while (spaces[choice].textContent !== '');
+    const formData = new FormData(event.target);
+    const formObj = Object.fromEntries(formData.entries());
+    const formName = formObj.name;
 
-    return choice
-}
-
-(function() {
-    const spaces = document.querySelectorAll('.space');
-    const userIco = 'X';
-    const opIco = 'O';
-
-    for (let i = 0; i < spaces.length; i++) {
-        spaces[i].addEventListener('click', () => {
-            if (spaces[i].textContent === '') {
-                spaces[i].textContent = userIco;
-            }
-        });
+    function createUser(name) {
+        const userName = name;
+        let userScore = 0;
+        const getUserScore = () => userScore;
+        const giveUserScore = () => userScore++;
+    
+        return { userName, userScore, getUserScore, giveUserScore }
     }
 
-    selectSpace();
-    spaces[selectSpace()].textContent = opIco;
-    console.log(selectSpace());
-})();
+    function createOp(name) {
+        const opName = name;
+        let opScore = 0;
+        const getOpScore = () => opScore;
+        const giveOpScore = () => opScore++;
+    
+        return { opName, opScore, getOpScore, giveOpScore }
+    }
+
+    const user = createUser(formName);
+    const opponent = createOp('Opponent');
+
+    console.log(user);
+    console.log(opponent);
+}
+
+// Opponent logic
+
+// function selectSpace (choice) {
+//     do {
+//         choice = game.board[(Math.floor(Math.random() * game.board.length))];
+//     } while (choice.textContent !== '');
+
+//     // choice.textContent = 'O';
+//     console.log(choice);
+
+//     return choice
+// }
+
+// Game flow
+
+// (function(spaces) {
+//     spaces = game.board;
+//     const userIco = 'X';
+//     const opIco = 'O';
+
+//     for(let i = 0; i < spaces.length; i++) {
+//         spaces[i].addEventListener('click', () => {
+//             if(spaces[i].textContent === '') {
+//                 spaces[i].textContent = userIco;
+//             }
+//             selectSpace().textContent = opIco;
+//         });
+//     }
+// })();
